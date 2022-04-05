71bf8b41fe0c341aa02f986d0a67df0fec901759
C-23-sayali <103025904+C-23-sayali@users.noreply.github.com>
2022-04-05 13:06:18 +0530

    Add files via upload

---
+++ my first program s.c
@@ -0,0 +1,30 @@
+// my first c program
+
+#include <stdio.h>
+
+int main ()
+
+ 
+{
+printf("talk to a teacher \n");
+    printf("welcome\n");
+     
+return 0;
+
+}
+
+
+
+output :
+talk to a teacher 
+welcome
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+
+
+
+
+
+
